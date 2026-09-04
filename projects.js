4b36357dff6773d8fd4f8ac6906a5dd20f1c2f55
fluxdium <fluxdium@gmail.com>
2026-09-04 19:19:54 +0900

    Move portfolio entries into managed project data

---
+++ projects.js
@@ -0,0 +1,79 @@
+/*
+ * FLUX portfolio data
+ * Add or update portfolio entries here. The page markup is generated automatically.
+ */
+window.FLUX_PROJECTS = [
+  {
+    category: 'mechanism',
+    videoId: '5gJfvawTQjY',
+    image: './src/flux_work_01.jpg',
+    alt: '제이시스메디칼 고주파 멜라닌 억제 기전 시각화',
+    title: '제이시스메디칼 고주파 멜라닌 억제 기전',
+    description: '고주파의 멜라닌 합성 억제, 자가포식 유도, 림프관 신생을 통한 색소침착 개선 기전 시각화.'
+  },
+  {
+    category: 'device',
+    videoId: 'RxNmCo08GjQ',
+    image: './src/flux_work_02.jpg',
+    alt: '제이시스메디칼 덴서티 알파팁 의료기기 홍보 영상',
+    title: '제이시스메디칼 덴서티 알파팁',
+    description: '기기 외관 뷰티샷 및 피부 단면을 투과하는 알파팁의 작동 원리를 설명하는 홍보 영상.'
+  },
+  {
+    category: 'medical',
+    videoId: 'z2G8yPKpNyI',
+    image: './src/flux_work_03.jpg',
+    alt: '자생한방병원 안면신경마비 치료 3D 애니메이션',
+    title: '자생한방병원 안면신경마비 치료 기전',
+    description: '안면부 추나요법, 침, 약침, 한약 등 안면신경마비의 단계별 치료 및 회복 과정을 설명하는 시각화 영상.'
+  },
+  {
+    category: 'medical',
+    videoId: 'Y-4Nry1GZ6A',
+    image: './src/flux_work_04.jpg',
+    alt: '자생한방병원 사고 후유증 신경 근육 손상 메디컬 CG',
+    title: '자생한방병원 사고 후유증 신경 및 근육 손상',
+    description: '사고의 물리적 충격으로 인한 신경 및 근육 손상 과정 원리를 설명하는 시각화 영상.'
+  },
+  {
+    category: 'commercial',
+    videoId: 'E4_KZ7a97Rk',
+    image: './src/flux_work_05.jpg',
+    alt: '기아 EV3 3D 커머셜 영상',
+    title: '기아 EV3 2025 World Car Awards',
+    description: '기아 EV3, EV5, EV9 3년 연속 월드카어워즈 수상 프로모션 영상.'
+  },
+  {
+    category: 'commercial',
+    videoId: '-6mSHPxPTxY',
+    image: './src/flux_work_06.jpg',
+    alt: '애경 리큐 제트 프로 브랜드 필름',
+    title: '애경 리큐 제트 프로',
+    description: '애경 세탁 세제 리큐 제트 프로의 주요 특장점을 직관적으로 풀어낸 제품 프로모션 영상.'
+  },
+  {
+    category: 'commercial',
+    videoId: 'OSR6bzS874c',
+    image: './src/flux_work_07.jpg',
+    alt: '세종특별자치시 세종글꽃체 타이틀 영상',
+    title: '세종특별자치시 세종글꽃체',
+    description: '세종시 전용 서체의 형태와 의미를 소개하는 타이틀 영상.'
+  },
+  {
+    category: 'commercial',
+    videoId: 'JkaYx4_ue6A',
+    image: './src/flux_work_09.jpg',
+    alt: '국가평생교육진흥원 K-MOOC 프로모션 영상',
+    title: '국가평생교육진흥원 K-MOOC',
+    description: '종편 채널 송출용 K-MOOC 브랜드 캠페인 광고 영상 제작.'
+  },
+  {
+    category: 'commercial',
+    videoId: 'EIdp0ADl10Y',
+    image: './src/flux_work_10.jpg',
+    alt: '한겨레 휴먼테크놀로지어워드 3D 타이틀 패키지',
+    title: '한겨레 휴먼테크놀로지어워드 타이틀 패키지',
+    description: '시상식 상징 오브제를 활용하여 제작한 어워드 수상 영상.'
+  }
+];
+
